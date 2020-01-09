5f406ffb32f6f184b08b746988234ab9b4fa74cd
alingenhag <11538311+alingenhag@users.noreply.github.com>
2020-01-09 21:04:47 +0100

    Use maven profiles.

--- app-slave/src/main/java/ch/fuzzle/lucenetest/configuration/JaxRsConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ch.fuzzle.lucenetest.configuration;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class JaxRsConfiguration extends Application {
-    // nothing to configure
-}
--- app-slave/src/main/resources/db/migration/V1__Add_person_table.sql
+++
@@ -1,6 +0,0 @@
-create sequence person_seq;
-create table  person (
-    id integer primary key,
-    firstname varchar(250),
-    lastname varchar(250)
-);
